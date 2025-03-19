4cc19f162761a00a102ef11b061ef34d7207deae
wiljanverhoeven <wiljanverhoeven@xs4all.nl>
2025-03-19 13:20:23 +0100

    start

--- first-app/resources/views/home.blade.php
+++ first-app/resources/views/home.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
-    <h1>banaan</h1>
+    <h1>Welcome</h1>
+    <a>Booking</a>
+    <a>Upcoming festivals</a>
+    <a>Make an account</a>
 </x-layout>
--- first-app/routes/web.php
+++ first-app/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('booking', function () {
+    return view('booking');
+});
+
+Route::get('payment', function () {
+    return view('payment');
+});
