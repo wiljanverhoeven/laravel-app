33fd9fca4a00d04712838c49ae0f9b796db8cbf9
wiljanverhoeven <wiljanverhoeven@xs4all.nl>
2025-03-19 12:25:51 +0100

    meow

---
+++ first-app/resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+
+    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        {{ $slot }}
+    </body>
+</html>
